adb8c4645273f23374a978a60f403c86bb35f5a6
Alexandre Possebom <alexandrepossebom@gmail.com>
2013-03-29 17:14:28 -0300

    - Fixed

--- src/com/possebom/mymedicines/test/TestMain.java
+++ src/com/possebom/mymedicines/test/TestMain.java
@@ -4,6 +4,7 @@ import android.graphics.Point;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
 
 import com.jayway.android.robotium.solo.Solo;
 import com.possebom.mymedicines.MedicineListActivity;
@@ -14,6 +15,7 @@ public class TestMain extends ActivityInstrumentationTestCase2<MedicineListActiv
 
 	private Solo solo;
 	private String item;
+	private View view;
 
 	public TestMain() {
 		super(MedicineListActivity.class);
@@ -25,29 +27,49 @@ public class TestMain extends ActivityInstrumentationTestCase2<MedicineListActiv
 		solo = new Solo(getInstrumentation(), getActivity());
 		item = String.format("Item %03d", 1);
 	}
+	
+	public void testInsertEmpty(){
+		String text = solo.getString(R.string.alert_title_medicine);
+		view = solo.getView(R.id.menu_add);
+		solo.waitForView(view);
+		solo.clickOnActionBarItem(R.id.menu_add);
+		view = solo.getView(R.id.menu_save);
+		solo.waitForView(view);
+		solo.clickOnActionBarItem(R.id.menu_save);
+		assertTrue("Title not found",solo.waitForText(text));
+	}
+	
+	public void testBarcode(){
+		String text = solo.getString(R.string.title_alert_barcodescanner);
+		view = solo.getView(R.id.menu_add);
+		solo.waitForView(view);
+		solo.clickOnActionBarItem(R.id.menu_add);
+		view = solo.getView(R.id.menu_barcode);
+		solo.waitForView(view);
+		solo.clickOnActionBarItem(R.id.menu_barcode);
+		assertTrue("Title not found",solo.waitForText(text));
+	}
 
 	public void testAddItem(){
-		Log.d("Teste","Creating item : "+item);
+		view = solo.getView(R.id.menu_add);
+		solo.waitForView(view);
 		solo.clickOnActionBarItem(R.id.menu_add);
+		view = solo.getView(R.id.menu_save);
+		solo.waitForView(view);
 		solo.enterText(0, item);
 		solo.clickOnActionBarItem(R.id.menu_save);
+		assertTrue(solo.searchText(item));
+		solo.clickOnText(item);
+		assertTrue(solo.searchText(solo.getString(R.string.validity)));
+		solo.clickOnActionBarHomeButton();
 		assertTrue(solo.searchText(item));
 	}
 
 	public void testRemoveItem(){
-		Display display = getActivity().getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int screenWidth = size.x;
-		int screenHeight = size.y;
-		
-		int fromX = (screenWidth/2) - (screenWidth/3);
-		int toX = (screenWidth/2) + (screenWidth/3);
-
-		int fromY = screenHeight/5;
-		int toY = fromY;
-		
-		solo.drag(fromX, toX, fromY, toY, 1);
+		solo.clickOnText(item);
+		view = solo.getView(R.id.menu_delete);
+		solo.waitForView(view);
+		solo.clickOnActionBarItem(R.id.menu_delete);
 		assertFalse(solo.searchText(item));
 	}
 
