8fe31fe34f0f302c7ce5ca8e02acce938e560c29
Alexandre Possebom <alexandrepossebom@gmail.com>
2013-03-30 00:27:34 -0300

    - Added and Remove in one test

--- src/com/possebom/mymedicines/test/TestMain.java
+++ src/com/possebom/mymedicines/test/TestMain.java
@@ -20,44 +20,32 @@ public class TestMain extends ActivityInstrumentationTestCase2<MedicineListActiv
 	protected void setUp() throws Exception {
 		super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
-		item = String.format("Item %03d", 1);
+		item = String.format("Item %d", System.currentTimeMillis());
 	}
 	
 	public void testInsertEmpty(){
 		String text = solo.getString(R.string.alert_title_medicine);
-		getInstrumentation().waitForIdleSync();
 		solo.clickOnActionBarItem(R.id.menu_add);
-		getInstrumentation().waitForIdleSync();
 		solo.clickOnActionBarItem(R.id.menu_save);
 		assertTrue("Title not found",solo.waitForText(text));
 	}
 	
 	public void testBarcode(){
 		String text = solo.getString(R.string.title_alert_barcodescanner);
-		getInstrumentation().waitForIdleSync();
 		solo.clickOnActionBarItem(R.id.menu_add);
-		getInstrumentation().waitForIdleSync();
 		solo.clickOnActionBarItem(R.id.menu_barcode);
 		assertTrue("Title not found",solo.waitForText(text));
 	}
 
-	public void testAddItem(){
-		getInstrumentation().waitForIdleSync();
+	public void testAddAndRemoveItem(){
 		solo.clickOnActionBarItem(R.id.menu_add);
-		getInstrumentation().waitForIdleSync();
 		solo.enterText(0, item);
 		solo.clickOnActionBarItem(R.id.menu_save);
 		assertTrue(solo.searchText(item));
 		solo.clickOnText(item);
 		assertTrue(solo.searchText(solo.getString(R.string.validity)));
 		solo.clickOnActionBarHomeButton();
-		assertTrue(solo.searchText(item));
-	}
-
-	public void testRemoveItem(){
-		getInstrumentation().waitForIdleSync();
 		solo.clickOnText(item);
-		getInstrumentation().waitForIdleSync();
 		solo.clickOnActionBarItem(R.id.menu_delete);
 		assertFalse(solo.searchText(item));
 	}
